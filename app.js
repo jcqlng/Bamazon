b87895bcc86861e82a1f634e652adb3fa22ae57d
jcqlng <jcqlngeiger@outlook.com>
2020-06-30 12:46:23 -0400

    Finally got it to work properly

--- app.js
+++ app.js
@@ -1,168 +1,114 @@
 var inquirer = require('inquirer');
-var mysql      = require('mysql');
+var mysql = require('mysql');
+const util = require('util');
 
-var main = function() {
-    //set up private variables
-    var connection = getConnection();
+
+var main = async function(){
+
+    const db = makeDb(getConnection());
     
-    //display the current inventory
-    displayData(connection);
-
-
-    //ask for user input
-    /* var questions = [
-        {
+    try {
+      const rows = await db.query('SELECT * FROM bamazon.product');
+      buildDisplay(rows);
+      inquirer
+        .prompt([
+          {
             type: 'input',
-            name: 'productId',
-            message: 'Please select a product by Product Nr.',
-            validate: function(value) {
-                var valid = !isNaN(parseFloat(value));
-                return valid || 'Please enter a product number';
-              },
-            filter: Number
-        },
-        {
+            name: 'product',
+            message: 'What product do you want to order?',
+            
+          },
+          {
             type: 'input',
             name: 'quantity',
-            message: 'How many items would you like to buy?',
-            validate: function(value) {
-                var valid = !isNaN(parseFloat(value));
-                return valid || 'Please enter a quantity number';
-              },
-            filter: Number
-        }
-    ]
-
-    inquirer.prompt(questions).then(answers => {
-        
-        console.log(answers);
-    }); */
-
-    //perform the requested data operation
-
-    //run main again to return to the beginning
-    
-}
-
-    
-    
-
-
-// build a connection object to mysql
-var getConnection = function(){
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'application',
-        password : 'Password123',
-        database : 'bamazon'
-      });
-       
-      connection.connect();
-
-      return connection;
-}
-
-// get data function to retrieve the current
-// inventory from the database
-
-
-// display function to "write" the inventory as 
-// a table to the screen.
-var displayData = function(connection) {
-
-    var query = 'SELECT * FROM bamazon.product';
-    
-    
-    connection.query(query, function(error, results, fields){
-        if(error) throw error;
-        
-        var data =  {records: results, fields: fields};
-
-        console.log('------------------------------------------------------------------------------------------');
-        console.log('| Prod. Nr. | Product Name                | Department Name    | Stock     | Price       |');
-        console.log('------------------------------------------------------------------------------------------');
-        
-        for(var i = 0; i < data.records.length; i++){
+            message: 'How many do you want to order?',
             
-            console.log('|' 
-                + formatField(data.records[i].item_id, 11) + '|' + formatField(data.records[i].product_name, 30) + '|' + formatField(data.records[i].department_name, 20) + '|' + formatField(data.records[i].stock_quantity, 11) + '|' + formatField(data.records[i].price, 12) + '|');
-            console.log('------------------------------------------------------------------------------------------');
-        }
+          }
+        ])
+        .then(async (answers) => {
+          console.log('Placing your order...');
+          
+          await placeOrder(answers.product, answers.quantity, db);
+          main();
+        });
+    } catch (err) {
+      console.log(err);
+    } 
+      
+    
+
+            
+    
+
+}
+
+
+const getConnection = function(){
+  let connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'application',
+      password : 'Password123',
+      database : 'bamazon'
+    });
+     
+    connection.connect();
+
+    return connection;
+}
+
+const makeDb = function(connection) {
+  //const sql = ;
+    return {
+      query(sql, args) {
+        return util.promisify(connection.query).call(connection, sql, args);
+      },
+      close() {
+        return util.promisify(connection.end).call(connection);
+      }
+      
+    };
+}
+
+
+const buildDisplay = function(data){
+
+    console.log('------------------------------------------------------------------------------------------');
+    console.log('| Prod. Nr. | Product Name                | Department Name    | Stock     | Price       |');
+    console.log('------------------------------------------------------------------------------------------');
+
+    data.forEach(function(row){
+      console.log('|'+ formatField(row.item_id, 11) + '|' + formatField(row.product_name, 30) + '|' + formatField(row.department_name, 20) + '|' + formatField(row.stock_quantity, 11) + '|' + formatField(row.price, 12) + '|');
+      console.log('------------------------------------------------------------------------------------------');
     });
 
-    
-    
-    
-    var fieldLengths = [11, 30, 20, 11, 12];
-    
-    // for(var i = 0; i < data.records.length; i++){
-
-    // }
 }
 
-// get the user input
-var getUserInput = function(){
-    var questions = [
-        {
-            type: 'input',
-            name: 'productId',
-            message: 'Please select a product by Product Nr.',
-            validate: function(value) {
-                var valid = !isNaN(parseFloat(value));
-                return valid || 'Please enter a product number';
-              },
-            filter: Number
-        },
-        {
-            type: 'input',
-            name: 'quantity',
-            message: 'How many items would you like to buy?',
-            validate: function(value) {
-                var valid = !isNaN(parseFloat(value));
-                return valid || 'Please enter a quantity number';
-              },
-            filter: Number
-        }
-    ]
+const formatField = function(value, fieldLength){
+  var formatted = [fieldLength];
+  
+  var stringified = value.toString();
 
-    inquirer.prompt(questions).then(answers => {
-        
-        return answers;
-      });
+  for(var i = 0; i < (fieldLength); i++){
+      if(i === 0){
+          formatted[i] = ' ';
+      }else {
+          if(stringified[i-1]) {
+              formatted[i] = stringified[i-1];
+          }else{
+              formatted[i] = ' ';
+          }
+      }
+  }
+
+  return formatted.join('');
 }
 
-// place an order based on user selection
-// of product
-var placeOrder = function(id, quantity, connection){
+const placeOrder = async function(id, quantity, db){
+  const getQuantity = await db.query(`SELECT stock_quantity FROM bamazon.product WHERE item_id = ${id}`);
+  const newQuantity = getQuantity[0].stock_quantity - quantity;
+  //console.log(newQuantity)
+  const updateInventory = await db.query(`UPDATE bamazon.product SET stock_quantity = ${newQuantity} WHERE item_id = ${id}`);
 
 }
 
-// update the inventory after an order has been place 
-// for a specified product.
-var updateInventory = function(id, quantity, connection) {
-
-}
-
-var formatField = function(value, fieldLength){
-    var formatted = [fieldLength];
-    
-    var stringified = value.toString();
-
-    for(var i = 0; i < (fieldLength); i++){
-        if(i === 0){
-            formatted[i] = ' ';
-        }else {
-            if(stringified[i-1]) {
-                formatted[i] = stringified[i-1];
-            }else{
-                formatted[i] = ' ';
-            }
-        }
-    }
-
-    return formatted.join('');
-}
-
-
-//start the program by running the main function.
 main();
